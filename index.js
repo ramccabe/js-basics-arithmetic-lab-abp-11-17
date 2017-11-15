1964f80ccf71e5a2294f172b98cb92550a65eea9
Robert McCabe <robertadammccabe@gmail.com>
2017-11-15 20:40:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ const ageIsValid = currentAge === Number.isInteger();
 
 const randomNumber = (Math.random() * 20) + 0;
 
-//const randomInteger = Math.floor(Math.random());
+const randomInteger = Math.floor(Math.random());
 
-//const randomUserID = Math.random() * 1000000001) + 1000000020)
+const randomUserID = Math.random() * 1000000001) + 1000000020)
